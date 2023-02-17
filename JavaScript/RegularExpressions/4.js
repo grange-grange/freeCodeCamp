09043e083f2968898adc06bf5368beb675640cec
Aliaksandr Fisiuk <123926431+grange-grange@users.noreply.github.com>
2023-02-17 08:38:42 +0300

    .

--- JavaScript/RegularExpressions/4.js
+++ JavaScript/RegularExpressions/4.js
@@ -1,2 +1,8 @@
 //Ignore Case While Matching
 
+<<<<<<< Updated upstream
+=======
+let myString = "freeCodeCamp";
+let fccRegex = /freecodecamp/i; // Change this line
+let result = fccRegex.test(myString);
+>>>>>>> Stashed changes
