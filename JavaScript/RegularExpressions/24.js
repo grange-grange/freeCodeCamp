cf396283ace9e699c4e3b0afe11154740b062bb8
Alexsander Fisiuk <alexanderfisiuk@gmail.com>
2023-02-19 12:10:23 +0300

    .

---
+++ JavaScript/RegularExpressions/24.js
@@ -0,0 +1,5 @@
+//Match Non-Whitespace Characters
+
+let sample = "Whitespace is important in separating words";
+let countNonWhiteSpace = /\S/gi; // Change this line
+let result = sample.match(countNonWhiteSpace);
