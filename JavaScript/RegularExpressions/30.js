8cbdef8465e4e3092511299abf2966374c4f46d6
Aliaksandr Fisiuk <123926431+grange-grange@users.noreply.github.com>
2023-02-20 10:38:45 +0300

    .

---
+++ JavaScript/RegularExpressions/30.js
@@ -0,0 +1,6 @@
+//Regular Expressions
+
+let myString = "Eleanor Roosevelt";
+let myRegex = /(Eleanor|Franklin)\s(\w.\s)*Roosevelt/; // Change this line
+let result = myRegex.test(myString); // Change this line
+// After passing the challenge experiment with myString and see how the grouping works
---
+++ JavaScript/RegularExpressions/31.js
@@ -0,0 +1,5 @@
+//Reuse Patterns Using Capture Groups
+
+let repeatNum = "42 42 42";
+let reRegex = /^(\d+) \1 \1$/; // Change this line
+let result = reRegex.test(repeatNum);
